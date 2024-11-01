82b962ffffd9f1e06e1300fcd8fc177529ef9065
Ruan Kysa Mendes Bueno <160552107+RuanKysa@users.noreply.github.com>
2024-11-01 17:44:43 -0300

    CARRINHO LAYOUT

---
+++ src/pages/carrinho.js
@@ -0,0 +1,15 @@
+import Layout from "@/layout/layout"
+
+export default function Carrinho() {
+    return (
+        <Layout>
+            <div>
+                <h1>Carrinho</h1>
+                <div className="carrinho">
+                    <div className="item-carrinho">
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    )
+}
